647fafb2afa9a664747736a10748f1eeed499fd6
Nithin Vijayakumar <nv2ba@virginia.edu>
2020-07-22 21:53:14 -0400

    added readme

---
+++ README.md
@@ -0,0 +1,1 @@
+# Autograd Implementation in C++
---
+++ output/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  output
+//
+//  Created by Nithin Vijayakumar on 7/22/20.
+//  Copyright © 2020 Nithin Vijayakumar. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
